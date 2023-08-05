cd62ac95a9698753f30572d3c2ea1dd29b16cb3a
George Kasparyants <gkasparyants@gmail.com>
2023-08-05 17:06:18 +0300

    data request base class

--- datalake/data_request.py
+++ datalake/data_request.py
@@ -8,3 +8,13 @@ class DataRequest(object):
 
     def __repr__(self):
         return f"DataRequest({self.request_id})"
+
+    def retrieve(self, n=10):
+        pass
+
+    def get_view_url(self):
+        pass
+
+    def wait(self, n=100,
+             timeout=10):
+        pass
