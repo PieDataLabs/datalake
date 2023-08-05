05db1e65746c2dab33ec1410d56e7345027c64f5
George Kasparyants <gkasparyants@gmail.com>
2023-08-05 15:30:21 +0300

    stats & limits command

--- datalake/searcher.py
+++ datalake/searcher.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import requests
+import json
 
 
 class Searcher(object):
@@ -7,10 +9,30 @@ class Searcher(object):
         self.email = email
         self.api_key = api_key
 
+    def pierequest(self, endpoint, **data):
+        return requests.post("https://console.piedata.ai/api/process/proxy",
+                             files={
+                                 "piedemo__proxy": (None, json.dumps({
+                                     "project_id": "64b61f572e9765980a0640d3",
+                                     "direct": endpoint,
+                                     "method": "post",
+                                 })),
+                                 "__pie_json_data": (None, json.dumps({
+                                     "email": self.email,
+                                     "api_key": self.api_key,
+                                     **data
+                                 })),
+                             }).json()
+
     def limits(self):
-        pass
+        response = self.pierequest("/limits")
+        if response.get("status") != "ok":
+            print(response.get("message"))
+            exit()
+        return response.get("limits", {})
 
     def search(self, query,
                images,
                annotations):
-        return []
+        print(self.pierequest("/limits"))
+
--- scripts/datalaker.py
+++ scripts/datalaker.py
@@ -21,6 +21,9 @@ def parse_args():
                                action="append",
                                dest="annotations")
 
+    stats_parser = subparsers.add_parser("stats")
+    stats_parser.add_argument("--limits", action="store_true")
+
     args = parser.parse_args()
     return args
 
@@ -46,6 +49,19 @@ Paste here email and api key:
     email = input("email: ")
     api_key = input("API_KEY: ")
     save_credentials(email, api_key)
+    print("""
+Credentials successfully saved!
+""")
+
+
+def run_stats(limits: bool = False):
+    credentials = load_credentials()
+    if credentials is None:
+        print("ERROR: credentials are invalid.\nUse `datalake auth` command first.\n")
+        exit()
+    searcher = Searcher(**credentials)
+    if limits:
+        print(searcher.limits())
 
 
 def main(command, **kwargs):
@@ -53,6 +69,8 @@ def main(command, **kwargs):
         run_search(**kwargs)
     elif command == "auth":
         run_auth(**kwargs)
+    elif command == "stats":
+        run_stats(**kwargs)
     else:
         raise NotImplementedError()
 
