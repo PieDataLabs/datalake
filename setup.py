222f240e7b21a032ea0b7b8774599afd2e623df5
George Kasparyants <gkasparyants@gmail.com>
2023-08-11 22:40:40 +0300

    V1.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='piedatalake',
-    version='1.0.0',
+    version='1.0.1',
     python_requires=f'>=3.6,<3.9',
     description='Datalake search engine',
     url='https://github.com/PieDataLabs/datalake',
