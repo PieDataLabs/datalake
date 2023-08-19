2f103ebaa04ab9752447a13de93c274fa9717e90
George Kasparyants <gkasparyants@gmail.com>
2023-08-19 08:09:52 +0300

    dataset add new photos

---
+++ tests/test_dataset_add.py
@@ -0,0 +1,14 @@
+from datalake.searcher import Searcher
+from datalake.credentials import load_credentials
+from datalake.annotations import Tag, Polygon
+
+
+credentials = load_credentials()
+searcher = Searcher(**credentials)
+
+ds = searcher.dataset_list()[-1]
+print(ds)
+
+data_request = searcher.search("Cats")
+data_request.wait()
+ds.add(data_request, ids=[0, 1])
