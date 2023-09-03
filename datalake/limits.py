8feba65ec35453bea22f92f737d9fa39e6bf91f7
George Kasparyants <gkasparyants@gmail.com>
2023-09-03 17:54:27 +0300

    Get stats of public images search

--- datalake/limits.py
+++ datalake/limits.py
@@ -7,6 +7,9 @@ class Limits(object):
         self.data = data
 
     def __repr__(self):
+        n_public_datasets = self.data.get("n_public_datasets", 0)
+        n_public_images = self.data.get("n_public_images", 0)
+
         dq = self.data.get("download_quote", -1)
         if dq == -1:
             dq = "Unlimited"
@@ -18,6 +21,8 @@ class Limits(object):
         else:
             sq = f"{sq} per hour"
         return f"""
+N Public Datasets: {n_public_datasets}
+N Public Images: {n_public_images}
 Download Quota: {dq}
 Searches Quota: {sq}
 """
